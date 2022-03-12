eef3fa8e5bf9608c9ba0518170d38611ee52d066
Hammad Hassan <i121976@nu.edu.pk>
2022-03-13 00:11:43 +0500

    changes in color

--- sketch.js
+++ sketch.js
@@ -1,22 +1,25 @@
-let r = 400;
+var r = 350;
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
   colorMode(RGB);
 
-  strokeWeight(3);
+  strokeWeight(2);
   noFill();
 }
 
 function draw() {
-  background(1);
-  stroke(199, 80, 88);
+  background(10, 0, 50);
   orbitControl(4, 4);
 
   rotateY(90); 
   rotateZ(65);
-  let thetaM = map(mouseX, 0, width, 25, 75)
+  stroke(255)
+
+  let thetaM = map(mouseX, 0, width, 1, 10)
   let phiM = map(mouseY, 0, height, 1, 10)
   bumpySphere(thetaM, phiM);
 }
@@ -28,6 +31,7 @@ function normalSphere() {
       let x = r * cos(phi);
       let y = r * sin(phi) * sin(theta);
       let z = r * sin(phi) * cos(theta);
+      
       vertex(x, y, z);
     }
     endShape(CLOSE);
@@ -41,6 +45,7 @@ function bumpySphere(thetaM, phiM) {
       let x = r * (1+0.2*sin(theta*thetaM)*sin(phi*phiM)) * cos(phi);
       let y = r * (1+0.2*sin(theta*thetaM)*sin(phi*phiM)) * sin(phi) * sin(theta);
       let z = r * (1+0.2*sin(theta*thetaM)*sin(phi*phiM)) * sin(phi) * cos(theta);
+      // stroke(111+x, 191+y, 183+z);
       vertex(x, y, z);
     }
     endShape();
